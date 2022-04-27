4770571849d63c40f2f15913fcc500496065ecc0
Nicolas H <hubnersantos15@gmail.com>
2022-04-27 19:42:57 -0300

    #ADDED: Exercices 5,6

--- script.js
+++ script.js
@@ -16,19 +16,46 @@ function createCustomElement(element, className, innerText) {
 //   console.log(item.querySelector('span.item__sku').innerText);
 // }
 
+function calculatePrice() {
+  const spamCart = document.querySelectorAll('.total-price');
+  spamCart.forEach((item) => item.remove());
+  let price = 0;
+  const spamListPrice = document.querySelectorAll('.spanNone');
+  spamListPrice.forEach((item) => {
+  price += Number(item.textContent);
+  });
+  return price.toFixed(2);
+}
+function totalPrice() {
+  const sectionCart = document.querySelector('section.cart');
+  // console.log(sectionCart);
+  const priceTotal = document.createElement('span');
+  priceTotal.classList.add('total-price');
+  priceTotal.textContent = `R$${calculatePrice()}`;
+  sectionCart.appendChild(priceTotal);
+  // saveCartItems();
+}
 function cartItemClickListener(event) {
   // console.log('ronaldo');
   // console.log(event.target);
   const results = event.target;
   results.remove();
+  totalPrice();
+  saveCartItems();
 }
 function createCartItemElement({ sku, name, salePrice }) {
   const cart = document.querySelector('.cart__items');
   const li = document.createElement('li');
+  const spanLi = document.createElement('span');
+  spanLi.innerText = salePrice;
+  spanLi.classList.add('spanNone');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  li.appendChild(spanLi);
   cart.appendChild(li);
+  totalPrice();
+  saveCartItems();
 }
 async function fetchItemResults(parameter) {
   const results = await fetchItem(parameter);
@@ -68,6 +95,16 @@ async function fetchResults() {
 fecthProductsFor(await fetchProducts('computador'));
 }
 
+const emptyCart = document.querySelector('.empty-cart');
+emptyCart.addEventListener('click', () => {
+  const totalItems = document.querySelector('.cart__items');
+  totalItems.innerHTML = '';
+  totalPrice();
+  saveCartItems();
+});
+
 window.onload = () => {
   fetchResults();
+  getSavedCartItems();
+  totalPrice();
 };
